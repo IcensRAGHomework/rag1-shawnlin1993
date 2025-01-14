3e6a3039bdbe05e958e9b5ab99ed1b92f9d9d6c0
shawnlin1993 <shawn613408@gmail.com>
2025-01-14 18:09:43 +0800

    test02

--- student_assignment.py
+++ student_assignment.py
@@ -1,10 +1,12 @@
 import json
 import traceback
+from pprint import pprint
+from datetime import datetime  # 確保導入 datetime 模組
 
 from model_configurations import get_model_configuration
-
 from langchain_openai import AzureChatOpenAI
 from langchain_core.messages import HumanMessage
+
 
 gpt_chat_version = 'gpt-4o'
 gpt_config = get_model_configuration(gpt_chat_version)
@@ -23,21 +25,52 @@ def generate_hw04(question):
     
 def demo(question):
     llm = AzureChatOpenAI(
-            model=gpt_config['model_name'],
-            deployment_name=gpt_config['deployment_name'],
-            openai_api_key=gpt_config['api_key'],
-            openai_api_version=gpt_config['api_version'],
-            azure_endpoint=gpt_config['api_base'],
-            temperature=gpt_config['temperature']
+        model=gpt_config['model_name'],
+        deployment_name=gpt_config['deployment_name'],
+        openai_api_key=gpt_config['api_key'],
+        openai_api_version=gpt_config['api_version'],
+        azure_endpoint=gpt_config['api_base'],
+        temperature=gpt_config['temperature']
     )
     message = HumanMessage(
-            content=[
-                {"type": "text", "text": question},
-            ]
+        content=[
+            {"type": "text", "text": question},
+        ]
     )
     response = llm.invoke([message])
     
     return response
 
-def test0103(question):
-    pass
+def demo1(question):
+    llm = AzureChatOpenAI(
+        model=gpt_config['model_name'],
+        deployment_name=gpt_config['deployment_name'],
+        openai_api_key=gpt_config['api_key'],
+        openai_api_version=gpt_config['api_version'],
+        azure_endpoint=gpt_config['api_base'],
+        temperature=gpt_config['temperature'],          
+    )
+    message = HumanMessage(
+        content=[
+            {"type": "text", "text": question},
+        ]
+    )
+    # 呼叫 OpenAI API
+    response = llm.invoke([message])
+
+  # 構建所需的 JSON 結構
+    response_json = {
+        "Result": [
+            {
+                "date": datetime.now().strftime("%Y-%m-%d"),
+                "name": response
+            }
+        ]
+    }
+
+    return response
+
+if __name__ == '__main__':
+    response = demo1('2024年台灣10月紀念日有哪些?請根據json格式輸出')
+    pprint(response.content)
+  
